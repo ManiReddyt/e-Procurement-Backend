84d1b977d8fbde1c7eb85e31eae35178dbd03b6b
mani <tmanireddy2001@gmai..com>
2023-04-29 15:08:41 +0530

    interim

--- api.js
+++ api.js
@@ -1,18 +1,25 @@
-const events = require('events');
-const express = require('express')
-const buyerRoutes = require('./src/buyer/routes');
-const bidderRoutes = require('./src/bidder/routes');
-const adminRoutes = require('./src/admin/routes');
+const events = require("events");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const buyerRoutes = require("./src/buyer/routes");
+const bidderRoutes = require("./src/bidder/routes");
+const adminRoutes = require("./src/admin/routes");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const emitter = new events.EventEmitter()
-emitter.setMaxListeners(100)
+const emitter = new events.EventEmitter();
+emitter.setMaxListeners(100);
 
 app.use(express.json());
-app.use('/api/buyer',buyerRoutes);
-app.use('/api/bidder',bidderRoutes);
-app.use('/api/admin',adminRoutes);
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(port)
+app.use("/api/buyer", buyerRoutes);
+app.use("/api/bidder", bidderRoutes);
+app.use("/api/admin", adminRoutes);
+
+app.listen(port, () => {
+  console.log("server running on port ", port);
+});
--- dbconnection.js
+++ dbconnection.js
@@ -1,12 +1,12 @@
-const { password } = require('pg/lib/defaults')
+const { password } = require("pg/lib/defaults");
 
-const Pool = require('pg').Pool
+const Pool = require("pg").Pool;
 const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "EProcurement",
-    password: "Nithin@2001",
-    port: 5432,
+  host: "localhost",
+  user: "postgres",
+  port: 5432,
+  password: "9390348710",
+  database: "e-procurement",
 });
 
 module.exports = pool;
